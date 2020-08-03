d02ae310d5a4c7126077b1817feb37db05ff8713
helmut <wecke@uni-bremen.de>
2020-08-03 14:14:24 +0200

    Added the a function to read the input file

---
+++ fileio.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""Contains Routines for file I/O such as reading the data
+   from an input file and writing the results of the
+   Schrodinger's equation into an output file.
+"""
+
+import numpy as np
+
+def _read_schrodinger(inputfilepath):
+    """Reads the input file "schrodinger.inp" that has the user
+    defined data which describes the
+
+    Args:
+        inputfilepath: Path of the file which is going to be read
+
+    Raises:
+        FileNotFoundError: If input file could not be found
+        PermissionError:
+
+    Returns:
+        The different parameters
+    """
+
+    return
+
+
