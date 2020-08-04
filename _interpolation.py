d9c727ce5894d1a8cb505a0a0bfc1491556bc2c2
Keno Krieger <kriegerk@uni-bremen.de>
2020-08-04 11:29:11 +0200

    Refine tests

--- _interpolation.py
+++ _interpolation.py
@@ -8,7 +8,7 @@ def _interpolate(x, y, xopt, kind='linear'):
     Interpolates two given sets of data points by either linear, natural
     cubic spline or polynomial interpolation and returns an array of x-, and
     y-values according to the specified intervall.
-    
+
     Args:
         x (1darray): X-coordinates sorted in increasing order.
         y (1darray): Corresponding y-coordinates.
@@ -16,26 +16,27 @@ def _interpolate(x, y, xopt, kind='linear'):
             form (xmin, xmax, points).
         kind (str): The kind of interpolation to use. Accepted options are
             'linear', 'cspline' or 'polynomial'. Defaults to 'linear'.
-        
+
     Returns:
-        xint (array): The x-coordinates corresponding the computed 
+        xint (array): The x-coordinates corresponding the computed
             y-values.
         yint (array): The interpolated y-coordinates.
-        
+
     """
     legal_choices = ['linear', 'cspline', 'polynomial']
     if kind not in legal_choices:
-        msg = 'Invalid option {} for  interpolation, using default'
-        print(msg.format(kind))
+        msg = """Invalid option {} for  interpolation, using default.
+        Valid options are {}"""
+        print(msg.format(kind, legal_choices))
         kind = 'linear'
-        
+
     if kind == 'linear':
         intfunc = _linear(x, y)
     elif kind == 'cspline':
         intfunc = _cspline(x, y)
     else:
         intfunc = _poly(x, y)
-    
+
     xint = _genx(xopt)
     yint = _geny(xint, intfunc)
     return xint, yint
@@ -44,14 +45,14 @@ def _interpolate(x, y, xopt, kind='linear'):
 def _linear(x, y):
     """
     Uses linear interpolation to find a function matching a dataset.
-    
+
     Args:
         x (1darray): X-coordinates sorted in increasing order.
         y (1darray): Corresponding y-coordinates.
-        
+
     Returns:
         intfunc (function object): The interpolated function.
-    
+
     """
     intfunc = interp1d(x, y)
     return intfunc
@@ -61,14 +62,14 @@ def _cspline(x, y):
     """
     Uses natural cubic spline interpolation to find a function matching
     a dataset.
-    
+
     Args:
         x (1darray): X-coordinates sorted in increasing order.
         y (1darray): Corresponding y-coordinates.
-        
+
     Returns:
-        intfunc (PPoly): The interpolated function.    
-        
+        intfunc (PPoly): The interpolated function.
+
     """
     intfunc = CubicSpline(x, y)
     return intfunc
@@ -78,52 +79,51 @@ def _poly(x, y):
     """
     Uses polynomial interpolation to find a function matching
     a dataset.
-    
+
     Args:
         x (1darray): X-coordinates sorted in increasing order.
         y (1darray): Corresponding y-coordinates.
-        
+
     Returns:
-        intfunc (PPoly): The interpolated function.    
-        
+        intfunc (PPoly): The interpolated function.
+
     """
     intfunc = KroghInterpolator(x, y)
-    return intfunc    
+    return intfunc
 
 
 def _genx(xopt):
     """
     Generates an array of x-values matching the given minimum and maximum
     value.
-    
+
     Args:
         xopt (touple): Touple of form (xmin, xmax, points) where xmax is
-            always excluded. 
-            
+            always excluded.
+
     Returns:
         x (array): Array ranging from xmin to xmax of shape (points,)
-    
+
     """
     xmin, xmax, points = xopt
     x = linspace(xmin, xmax, points)
     return x
-    
-    
+
+
 def _geny(x, func):
     """
     Generates an array containing y-values corresponding to given x-coordinates
     by using a function y = f(x).
-    
+
     Args:
-        x (1darray): Array containing the x-values for which matching 
+        x (1darray): Array containing the x-values for which matching
             y-values shall be computed.
         func (callable object): The function used to generate the data.
-        
+
     Returns:
         y (1darray): Array containing y-values matching the supplied
             x-coordinates.
-    
+
     """
     y = func(x)
     return y
-    
--- test_interpolation.py
+++ test_interpolation.py
@@ -1,49 +1,58 @@
 #!/usr/bin/env python3
 """Tests for the private _interpolation module"""
 from _interpolation import _interpolate
-from numpy import array
+from numpy import array, arange, allclose
 import pytest
 
 POINTS = [5, 10, 100, 1000, 5000, 10000]
-
+XSHAPE = array([0, 7, 25, 100, 150])
+YSHAPE = array([-5, 3, 25, -5.5, 10])
+KINDS = ['linear', 'cspline', 'polynomial']
 
 @pytest.mark.parametrize('n', POINTS)
 def test_shape_lin(n):
     """Tests whether computed arrays have the correct shape"""
-    x = array([0, 7, 25, 100, 150])
-    y = array([-5, 3, 25, -5.5, 10])
     xopt = (0, 150, n)
-    xint, yint = _interpolate(x, y, xopt)
-    
+    xint, yint = _interpolate(XSHAPE, YSHAPE, xopt)
+
     if xint.shape != yint.shape:
-        assert xint.shape != yint.shape 
-    else:      
-        assert xint.shape[0] == n and yint.shape[0] == n  
+        assert xint.shape != yint.shape
+    else:
+        assert xint.shape[0] == n and yint.shape[0] == n
 
 
 @pytest.mark.parametrize('n', POINTS)
-def test_shape_csplin(n):
+def test_shape_cspline(n):
     """Tests whether computed arrays have the correct shape"""
-    x = array([0, 7, 25, 100, 150])
-    y = array([-5, 3, 25, -5.5, 10])
     xopt = (0, 150, n)
-    xint, yint = _interpolate(x, y, xopt, kind='csplin')
-    
+    xint, yint = _interpolate(XSHAPE, YSHAPE, xopt, kind='cspline')
+
     if xint.shape != yint.shape:
-        assert xint.shape != yint.shape 
-    else:      
-        assert xint.shape[0] == n and yint.shape[0] == n  
+        assert xint.shape != yint.shape
+    else:
+        assert xint.shape[0] == n and yint.shape[0] == n
 
 
 @pytest.mark.parametrize('n', POINTS)
 def test_shape_poly(n):
     """Tests whether computed arrays have the correct shape"""
-    x = array([0, 7, 25, 100, 150])
-    y = array([-5, 3, 25, -5.5, 10])
     xopt = (0, 150, n)
-    xint, yint = _interpolate(x, y, xopt, kind='polynomial')
-    
+    xint, yint = _interpolate(XSHAPE, YSHAPE, xopt, kind='polynomial')
+
     if xint.shape != yint.shape:
-        assert xint.shape != yint.shape 
-    else:      
-        assert xint.shape[0] == n and yint.shape[0] == n  
+        assert xint.shape != yint.shape
+    else:
+        assert xint.shape[0] == n and yint.shape[0] == n
+
+
+@pytest.mark.parametrize('kind', KINDS)
+def test_ifpivots(kind):
+    """Tests whether the interpolation goes through all pivots"""
+    pivots = array([-10., -7.5, -5., -2.5, 0., 2.5, 5., 7.5, 10.])
+    comparisons = arange(0, 2249, 250)
+
+    ycords = array([1, -10, 23, 5, 100, -5, 10, 20, 9])
+    xopt = (-10, 10, 2001)
+    xint, yint = _interpolate(pivots, ycords, xopt, kind=kind)
+
+    assert allclose(ycords, yint[comparisons])
