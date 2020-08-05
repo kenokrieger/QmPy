ede15978c219b71ef7debbba34246ab11ccc7382
helmut <wecke@uni-bremen.de>
2020-08-05 14:14:25 +0200

    Add function to read data files with the values for the plots

--- graphics.py
+++ graphics.py
@@ -14,8 +14,35 @@ def QM_Plottings(dirname):
         the four following files: potential.dat, energies.dat,
         wavefuncs.dat, and expvalues.dat.
 
-
     Return:
         Plots of the potential, the eigenvalues and the respective
     wave functions
     """
+    potdata, energiesdata, wavefuncsdata, expvaluesdata = _readplotsfiles(dirname)
+
+
+
+def _readplotsfiles(dirname):
+    """Reads the files and exports the data needed to use the QM_Plottings
+    function
+
+    Args:
+        dirname: Name of the directory or path from which
+        the files are going to be ploted. The directory must have
+        the four following files: potential.dat, energies.dat,
+        wavefuncs.dat, and expvalues.dat.
+    Returns:
+        potdata
+        energiesdata
+        wavefuncsdata
+        expvaluesdata
+    """
+    potpath = os.path.join(dirname, "potential.dat")
+    energiespath = os.path.join(dirname, "energies.dat")
+    wavefuncspath = os.path.join(dirname, "wavefuncs.dat")
+    expvaluespath = os.path.join(dirname, "expvalues.dat")
+    potdata =  np.loadtxt(potpath)
+    energiesdata = np.loadtxt(energiespath)
+    wavefuncsdata = np.loadtxt(wavefuncspath)
+    expvaluesdata = np.loadtxt(expvaluespath)
+    return potdata, energiesdata, wavefuncsdata, expvaluesdata
