dcfd61583040262d9526883ca71e1cc5dacf31d4
Keno Krieger <kriegerk@uni-bremen.de>
2020-08-06 22:26:07 +0200

    Shamelessly copy html theme from astropy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ author = 'Helmut Wecke, Keno Krieger'
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.napoleon'
-]
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -45,8 +45,8 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+# Needs external package astropy-sphinx-theme 1.1
+html_theme = 'bootstrap-astropy'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
