df8884fa24657d3bf057cee4545e1191843cbf29
Keno Krieger <kriegerk@uni-bremen.de>
2020-08-10 11:37:34 +0200

    Set up directory for PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qmpy-kriegerk", # Replace with your own username
+    version="0.0.1",
+    author="Keno Krieger, Helmut Wecke",
+    author_email="kriegerk@uni-bremen.de",
+    description="Package for numerical solving of the schroedinger equation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenokrieger/QmPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 2-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
