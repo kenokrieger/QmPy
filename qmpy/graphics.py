b8954050d445012a5eec5396674755725b39a9d1
Keno Krieger <kriegerk@uni-bremen.de>
2020-08-12 15:13:08 +0200

    Add autoscale option

--- qmpy/graphics.py
+++ qmpy/graphics.py
@@ -2,66 +2,53 @@
     respective wave functions as well as the expected values for each
     eigenvalue from the files that contain the solution of the problem.
 """
-import os
-import numpy as np
-from fileio import _readplotsfiles
 import matplotlib.pyplot as plt
+from qmpy.fileio import _readplotsfiles
 
 
-def qm_plottings(dirname, xmin, xmax, enmin, enmax, scale):
-    """Plots the potential, the eigenvalues and the respective
+def qm_plottings(dirname, auto_scale=True, scale=None, sname='qmpy_plot.pdf'):
+    """
+    Plots the potential, the eigenvalues and the respective
     wave functions as well as the expected values for each eigenvalue
     from the files that contain the solution of the problem
 
     Args:
         dirname: Name of the directory or path from which
-        the files are going to be ploted. The directory must have
-        the four following files: potential.dat, energies.dat,
-        wavefuncs.dat, and expvalues.dat.
-        xmin: Minimal value of the x-axis
-        xmax: Maximal value of the x-axis
-        enmin: Minimal value of the energy-axis
-        enmax: Maximal value of the energy-axis
-        scale: Factor to fit the wavefunctions
+            the files are going to be ploted. The directory must have
+            the four following files: potential.dat, energies.dat,
+            wavefuncs.dat, and expvalues.dat.
+        auto_scale (bool): Automatically scale the wavefunctions. Defaults to
+            True.
+        scale (float): Manually set a value for the scale factor. Defaults to
+            None.
+        sname (str): The name for the file to save the plot to. Defaults to
+            'qmpy_plot.pdf'.
 
     Return:
-        Plots of the potential, the eigenvalues and the respective
-        wave functions
+        The figure and axes of the plot. Where ax1 is the left and ax2 is the
+        right subplot.
 
     """
-    (xcoordsarray, potsarray, energarray, wfuncsarray,
-     expvalsarray, uncertainityarray) = _isolate_plot_data(dirname)
+    plot_data = _isolate_plot_data(dirname)
     fig = plt.figure(1)
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax1.set_xlabel("x [Bohr]")
-    ax1.set_ylabel("Energy [Hartree]")
-    ax1.set_title(r'Potential, eigenstates, $ \langle x \rangle $')
-    ax1.set(xlim=(xmin, xmax), ylim=(enmin, enmax))
-    ax1.plot(xcoordsarray, potsarray, color="black")
+    title = r'Potential, eigenstates, $ \langle x \rangle $'
+    ax1 = _make_subplot(fig, 121, title)
+    _plot_pot(ax1, plot_data)
 
-    wfuncsarray.T
-    ii = 0
-    for wfunc, energy in zip(wfuncsarray, energarray):
-        offsetwfunc = scale * wfunc + energy
-        if ii % 2 == 0:
-            ax1.plot(xcoordsarray, offsetwfunc, color="red")
-        else:
-            ax1.plot(xcoordsarray, offsetwfunc, color="blue")
-        ii += 1
+    if scale is None:
+        scale = 1
+        if auto_scale:
+            scale = _compscale(plot_data)
 
-    ax1.hlines(energarray, xcoordsarray[0], xcoordsarray[-1], color='grey')
-    ax1.scatter(expvalsarray, energarray, color="green", marker="x")
+    _plot_wfuncs(ax1, plot_data, scale)
+    _plot_expvals(ax1, plot_data)
 
-    ax2 = fig.add_subplot(1, 2, 2)
-    ax2.set_xlabel("x [Bohr]")
-    ax2.set_title(r'$\sigma_{x}$')
+    title = r'$\sigma_{x}$'
+    ax2 = _make_subplot(fig, 122, title)
+    _plot_unc(ax2, plot_data)
 
-    ax2.hlines(energarray, xcoordsarray[0], xcoordsarray[-1], color='grey')
-    ax2.scatter(uncertainityarray, energarray, color="purple",
-                marker="+")
-
-    plt.show()
-    # plt.savefig("QmPy_plots")
+    plt.savefig(sname)
+    return fig, ax1, ax2
 
 
 def _isolate_plot_data(dirname):
@@ -76,7 +63,7 @@ def _isolate_plot_data(dirname):
         wavefuncs.dat, and expvalues.dat.
 
     Return:
-        touple: The extracted  data.
+        dict: The extracted  data.
 
             - **xcoords** (*1darray*) - The x-coordinates,
 
@@ -95,11 +82,135 @@ def _isolate_plot_data(dirname):
 
     """
     potdata, energdata, wfuncsdata, expvaldata = _readplotsfiles(dirname)
-    xcoords = potdata[:, 0].copy()
-    pots = potdata[:, 1].copy()
-    energies = energdata
-    wfuncs = wfuncsdata[:, 1:].copy()
-    expvals = expvaldata[:, 0].copy()
-    uncertainties = expvaldata[:, 1].copy()
+    plot_data = dict()
+    plot_data['xcoords'] = potdata[:, 0].copy()
+    plot_data['pots'] = potdata[:, 1].copy()
+    plot_data['energies'] = energdata
+    plot_data['wfuncs'] = wfuncsdata[:, 1:].copy()
+    plot_data['expvals'] = expvaldata[:, 0].copy()
+    plot_data['uncertainties'] = expvaldata[:, 1].copy()
 
-    return xcoords, pots, energies, wfuncs, expvals, uncertainties
+    return plot_data
+
+
+def _make_subplot(fig, subplotnum, title):
+    """
+    Generates a subplot on the given figure.
+
+    Args:
+        fig (matplotlib.Figure): The figure to add the subplot to.
+        subplotnum (int): The options for the subplot. Needs to have 3 digits
+            where the first digit stands for the total number of rows, the
+            second for the number of columns and the third is the index for
+            the subplot.
+        title (str): The title for the plot.
+
+    Returns:
+        ax (matplotlib.Axes): The generated subplot object.
+
+    """
+    ax = fig.add_subplot(subplotnum)
+    ax.set_xlabel("x [Bohr]")
+    ax.set_ylabel("Energy [Hartree]")
+    ax.set_title(title)
+    return ax
+
+
+def _plot_expvals(ax, data):
+    """
+    Adds the expected values for the x-coordinate to a given subplot.
+
+    Args:
+        ax (matplotlib.Axes): The subplot to plot to.
+        data (dict): The data to plot.
+
+    Returns:
+        None.
+
+    """
+    xrange = (data['xcoords'][0], data['xcoords'][-1])
+    ax.hlines(data['energies'], xrange[0], xrange[-1], color='grey')
+    ax.scatter(data['expvals'], data['energies'], color="green", marker="x")
+
+
+def _plot_pot(ax, data):
+    """
+    Plots the values of the potential to a given subplot.
+
+    Args:
+        ax (matplotlib.Axes): The subplot to plot to.
+        data (dict): The data to plot.
+
+    Returns:
+        None.
+
+    """
+    ax.plot(data['xcoords'], data['pots'], color="black")
+
+
+def _plot_unc(ax, data):
+    """
+    Plots the uncertainty of the x-coordinate to a given subplot.
+
+    Args:
+        ax (matplotlib.Axes): The subplot to plot to.
+        data (dict): The data to plot.
+
+    Returns:
+        None.
+
+    """
+    xrange = (data['xcoords'][0], data['xcoords'][-1])
+    ax.hlines(data['energies'], xrange[0], xrange[-1], color='grey')
+    ax.scatter(data['uncertainties'], data['energies'],
+               color="purple", marker="+")
+
+
+def _plot_wfuncs(ax, data, scale):
+    """
+    Plots the wavefunctions and energy levels contained in data and applies
+    the given scale.
+
+    Args:
+        ax (matplotlib.Axes): The subplot to plot to.
+        data (dict): The data needed for the plot. Needs to have keys
+            'xcoords', 'wfuncs', 'energies'.
+
+    Returns:
+        None.
+
+    """
+    data['wfuncs'].T
+    ii = 0
+    for wfunc, energy in zip(data['wfuncs'], data['energies']):
+        offsetwfunc = scale * wfunc + energy
+        if ii % 2 == 0:
+            ax.plot(data['xcoords'], offsetwfunc, color="red")
+        else:
+            ax.plot(data['xcoords'], offsetwfunc, color="blue")
+        ii += 1
+
+
+def _compscale(data):
+    """
+    Automatically computes a scaling for the wavefunctions in the plot.
+
+    Args:
+        data (dict): The data neccesarry for computing the scale factor. Needs
+            to contain 'wfuncs' and 'energies'.
+
+    Returns:
+        scale (float): The computed scale.
+
+    """
+    wfuncs = data['wfuncs'].T
+    energies = data['energies']
+    scale = 1e6
+
+    for index in range(len(wfuncs) - 1):
+        new_scale = (energies[index + 1] - energies[index]) / abs(
+            (abs(min(wfuncs[index + 1])) - max(wfuncs[index])))
+        if new_scale < scale:
+            scale = new_scale
+
+    return scale
